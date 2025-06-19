49d74313bbaf1f9cfee09759c84001e92ff9e07b
Kofi K <kofi.k.otchere@gmail.com>
2025-06-19 18:22:37 +0000

    feat:42th i think im done adjusting sleepy harper. and some other stuff

--- enemy/sleepy_harper.gd
+++ enemy/sleepy_harper.gd
@@ -1,14 +1,12 @@
 extends enemy
 var shootOnce:bool=false
+var originalPos
 func _ready() -> void:
-	$AnimatedSprite2D.play("playing")
-	health=3
-
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity+=get_gravity()*delta
+	
+	health=2
+	playerdamagevalue=1
+	originalPos=Vector2(global_position.x,Player.playerCharacter.global_position.y)
 	calculate_player_distance()
-	spawn_collectables()
 	if distance_x<0:
 		$AnimatedSprite2D.flip_h=false
 		$AnimatedSprite2D.set_offset(Vector2(0,0))
@@ -19,13 +17,53 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.set_offset(Vector2(-9,0))
 		$hitbox/L.disabled=true
 		$hitbox/R.disabled=false
-	if $AnimatedSprite2D.frame==1:
-		if shootOnce==false:
-			var proj=preload("res://enemy/sleepy_harper_projectile.tscn").instantiate()
-			proj.position=position
-			get_tree().current_scene.add_child(proj)
-			shootOnce=true
-	else:shootOnce=false
+	if distance_x<0:
+		global_position.x=global_position.x+250
+		global_position.y=Player.playerCharacter.global_position.y
+	elif distance_x>=0:
+		global_position.x=global_position.x-250
+		global_position.y=Player.playerCharacter.global_position.y
+	#print(Player.playerCharacter.global_position.y)
+	#print(originalPos)
+
+func _physics_process(delta: float) -> void:
+	#print(global_position)
+	if (global_position!=originalPos):
+		var newx=move_toward(global_position.x,originalPos.x,250*delta)
+		#var newy=move_toward(global_position.y,originalPos.y,250*delta)
+		global_position.x=newx
+		#global_position.y=newy
+	#if not is_on_floor():
+		#velocity+=get_gravity()*delta
+	calculate_player_distance()
+	spawn_collectables()
+	if (global_position==originalPos):
+		$AnimatedSprite2D.play("playing")
+		if distance_x<0:
+			$AnimatedSprite2D.flip_h=false
+			$AnimatedSprite2D.set_offset(Vector2(0,0))
+			$hitbox/L.disabled=false
+			$hitbox/R.disabled=true
+		elif distance_x>=0:
+			$AnimatedSprite2D.flip_h=true
+			$AnimatedSprite2D.set_offset(Vector2(-9,0))
+			$hitbox/L.disabled=true
+			$hitbox/R.disabled=false
+		if $AnimatedSprite2D.frame:
+			if shootOnce==false:
+				var proj=preload("res://enemy/sleepy_harper_projectile.tscn").instantiate()
+				proj.position=position
+				get_tree().current_scene.add_child(proj)
+				shootOnce=true
+		#else:shootOnce=false
 
 func _on_shoot_projectile_timeout() -> void:
 	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	shootOnce=false
--- enemy/sleepy_harper_projectile.gd
+++ enemy/sleepy_harper_projectile.gd
@@ -2,16 +2,18 @@ extends enemy
 var foundPlayer:bool=false
 func _ready() -> void:
 	playerdamagevalue=2
-	
-
+	health=1
 func _physics_process(delta: float) -> void:
 	calculate_player_distance()
+	spawn_collectables()
+	if health<=0:
+		GlobalScript.spawn_collectable_no=100
 	if not foundPlayer:
 		if distance_x<0:
-			velocity.x=-1500*delta
+			velocity.x=-2200*delta
 		elif distance_x>=0:
-			velocity.x=1500*delta
-		velocity.y=randi_range(-1000,1000)*delta
+			velocity.x=2200*delta
+		velocity.y=randi_range(-1500,1500)*delta
 		$AnimatedSprite2D.frame=randi_range(0,4)
 		foundPlayer=true
 	
@@ -26,3 +28,10 @@ func _physics_process(delta: float) -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 	pass
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player_projectiles"):
+		#queue_free()
+		pass
+		
